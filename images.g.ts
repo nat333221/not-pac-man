63e6df94f8d8a14ad5dea55099dc0acb459e9c6b
nat333221 <93150596+nat333221@users.noreply.github.com>
2021-10-25 12:56:32 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "NOT_PAC_MAN":return img`
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 f 5 5 5 5 5 5 5 5 . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+. . . 5 5 5 5 5 5 5 5 5 5 5 5 5 
+. . . . 5 5 5 5 5 5 5 5 5 5 5 5 
+. . . . . 5 5 5 5 5 5 5 5 5 5 5 
+. . . . . . 5 5 5 5 5 5 5 5 5 5 
+. . . . . 5 5 5 5 5 5 5 5 5 5 5 
+. . . . 5 5 5 5 5 5 5 5 5 5 5 5 
+. . . 5 5 5 5 5 5 5 5 5 5 5 5 . 
+. . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+. . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
